9e045b36019a78eef145b665d198fe02109abc83
artwebs <arwebs@126.com>
2017-12-24 20:10:00 +0800

    import

---
+++ src/main/java/com/baidu/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.baidu;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootUeditorApplication.class);
+	}
+
+}
